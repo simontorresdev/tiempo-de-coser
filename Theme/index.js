74a3ee3f1ac7f27f137ef161644863af78f1e17c
simontorresdev <simontorres979797@gmail.com>
2021-11-06 17:31:58 -0500

    first commit

---
+++ Theme/index.js
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {
+    white: '#fff',
+    black: '#000',
+    red: '#f1374f',
+    title: '#2d2d2d',
+    text: '#535353'
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+})
+
---
+++ components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Heading } from "@chakra-ui/react"
+
+export const Header = () => {
+  return (
+    <div>
+      <Heading color="title">Header</Heading>
+    </div>
+  )
+}
